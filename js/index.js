731da2dd9f0d6b4f3daa6c0c0590e5a47e876dd2
yyoonseo93 <86402278+yyoonseo93@users.noreply.github.com>
2021-07-23 13:17:20 +0900

    v1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,50 @@
-$(".slider").slick({
+
+function init(){
+//start
+  const conSub1 = document.querySelector('.con-sub1');
+  const button = document.querySelectorAll('.button a');
+  
+  let res,tagList='',tagList2='',idx=0,num=0;
+  fetch('./js/data.json')
+    .then(res => res.json())
+    .then(data => callback(data));
+
+  function callback(data){
+      data.container2.forEach(function(v,k){
+          tagList += `
+                <div class="item">
+                    <div class="left">
+                        <h2><strong>${v.logo}</strong></h2>
+                        <p>
+                            <strong>${v.tit}</strong>
+                            ${v.detail}
+                        </p>
+                        <a href="living.html"><strong>${v.link}</strong></a>
+                    </div>
+                    <div class="right">
+                        <a class="Img" href="living.html"><img src="${v.img}" alt="livingimg"></a>
+                    </div>
+                </div>`;
+      });
+      conSub1.innerHTML = tagList;
+  }
+  for(let i=0; i<button.length; i++){
+    button[i].addEventListener('click',function(){
+      if(i == 1){
+        if(idx < 2) idx++;
+      }else{
+          if(idx > 0) idx--;
+      } 
+      let con = conSub1.offsetWidth * idx;
+      conSub1.style = `transform:translateX(${-con}px)`; 
+    });
+  }
+
+
+
+
+    
+  $(".slider").slick({
     arrows:false, /*false 화살표 사라짐*/
     dots: true, /* false 인디게이터 사라짐*/
     infinite: true,
@@ -10,5 +56,6 @@ $(".slider").slick({
     cssEase: 'linear'
   });
 
-
-  
+//end
+}
+window.onload = init;
